acd81e03707c701c0d643bd128c90d006d5861ea
Sara-H2F <r.0504127076+@gmail.com>
2026-08-31 15:44:49 +0300

    Fix CI test database configuration

--- java-backend/src/test/java/com/hand2font/Hand2fontApplicationTests.java
+++ java-backend/src/test/java/com/hand2font/Hand2fontApplicationTests.java
@@ -2,12 +2,13 @@ package com.hand2font;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Hand2fontApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
